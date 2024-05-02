3c14f00b41c432f421827fed7b855c3c6c94120a
Tamerlan13101975 <166745761+Tamerlan13101975@users.noreply.github.com>
2024-05-02 22:42:38 +0300

    add all characteristic

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 class Warrior():
-    def __init__(self, name, power, endurance, hair_color, info):
+    def __init__(self, name, power, endurance, hair_color):
         self.name = name
         self.power = power
         self.endurance = endurance
         self.hair_color = hair_color
-        self.info = info
+
 
     def sleep(self):
-        print(f"{self.name}"He feel a sleep")
+        print(f"{self.name}He feel a sleep")
         self.endurance += 2
 
 
     def eat(self):
-        print(f"{self.name}"He sat down to eat")
+        print(f"{self.name}He went to eat")
         self.power += 1
 
 
     def hit(self):
-        print(f"{self.name}"Ye strikes")
+        print(f"{self.name}He strikes")
         self.hit -= 5
 
 
     def walk(self):
-        print(f"{self.name}"He walks")
+        print(f"{self.name}He walks")
         self.endurance -= 5
 
 
@@ -32,6 +32,15 @@ class Warrior():
         print(f"Warrior's Hair Color - {self.hair_color}")
         print(f"Warrior's Endurance - {self.endurance}")
         print(f"Warrior's Power - {self.power}")
+
+
+War1 = Warrior("Kratos", 60, 80, "Brown")
+print(War1.name)
+print(War1.power)
+print(War1.endurance)
+print(War1.hair_color)
+
+
 
 
 
