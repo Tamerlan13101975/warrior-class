e77b1babdeeeccccbbff850dd378e672a0a2d986
Tamerlan13101975 <166745761+Tamerlan13101975@users.noreply.github.com>
2024-05-02 20:29:31 +0300

    new class code

---
+++ main.py
@@ -0,0 +1,8 @@
+class Warrior():
+    def __init__(self, name, power, endurance,hair_color):
+        self.name = name
+        self.power = power
+        self.endurance = endurance
+        self.hair_color = hair_color
+
+
